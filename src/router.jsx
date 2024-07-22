9c5090ef82fe42b83458b2aebcf9b7f7c389b472
baekcii <bsy8470@gmail.com>
2024-07-23 00:09:41 +0900

    1.0.18

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import MainPage from "./pages/MainPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/main",
+    element: <MainPage/>,
   },
 ]);
 
