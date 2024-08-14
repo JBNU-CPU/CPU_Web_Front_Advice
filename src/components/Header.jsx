81f0334b31341a994a2ca6fa8eb1b67bb820da9c
Louis Jeon <louisjeon@Louis-MacBook-Pro-16.local>
2024-08-14 19:47:12 +0900

    edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,57 @@ import styled from "styled-components";
 
 const StyledHeader = styled.div`
   height: 100px;
-  background: blue;
+  display: flex;
+  justify-content: space-around;
   h1 {
-    color: red;
+    color: #486284;
+    flex: 1;
+    text-align: center;
+    margin: auto;
+  }
+  ul {
+    flex: 4;
+    display: flex;
+    color: grey;
+    justify-content: space-between;
+    list-style-type: none;
+    margin: auto;
+  }
+  div {
+    margin: auto;
+    flex: 1.5;
+    display: flex;
+    justify-content: space-evenly;
+    height: 30px;
+    #joinBtn {
+      background: none;
+      border: none;
+      color: #486284;
+    }
+    #loginBtn {
+      border: none;
+      background: #486284;
+      color: white;
+      border-radius: 5px;
+    }
   }
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <h1>Header</h1>
+      <h1>CPU</h1>
+      <ul>
+        <li>ABOUT CPU</li>
+        <li>STUDIES</li>
+        <li>ACTIVITIES</li>
+        <li>BOARD</li>
+        <li>APPLY</li>
+      </ul>
+      <div>
+        <button id="joinBtn">JOIN</button>
+        <button id="loginBtn">Login</button>
+      </div>
     </StyledHeader>
   );
 }
