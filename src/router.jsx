19f7008cf65f2bb2396fc1a8925e054aba91c0d0
baekcii <bsy8470@gmail.com>
2024-08-20 02:35:59 +0900

    스터디 페이지

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Study from "./pages/Study"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/study",
+    element: <Study />,
   },
 ]);
 
