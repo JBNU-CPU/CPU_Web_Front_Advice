22ad6e4018d7234a906f6db68d794b46d74e0441
Louis Jeon <louisjeon@Louis-MacBook-Pro-16.local>
2024-08-20 14:17:30 +0900

    assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
 
+const StyledHome = styled.div`
+  .pictureContainer {
+    object-fit: cover;
+  }
+`;
+
 function Home() {
-  return <></>;
+  const [img, setImg] = useState(1);
+
+  return (
+    <StyledHome>
+      <div className="pictureContainer">
+        <img src={`main${img}`} alt="mainImg" />
+      </div>
+    </StyledHome>
+  );
 }
 
 export default Home;
